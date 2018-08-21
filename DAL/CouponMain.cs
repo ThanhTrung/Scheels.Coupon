1ab787d9d96d0d17e76d1aa61019c8ea271096a0
Thanh Trung <ngocthanh6383@yahoo.com>
2018-08-21 19:20:09 +0700

    Added DAL

---
+++ DAL/CouponMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class CouponMain
+    {
+        public int CouponID { get; set; }
+        public int GS1Code { get; set; }
+
+    }
+}
